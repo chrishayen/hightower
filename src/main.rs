17474a9ee3db8b01490c518a152db47b8e8e76c5
Chris Hayen <chris@shotgun.dev>
2025-10-03 11:55:15 -0400

    required HT_TOKEN

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use clap::Parser;
+use std::env;
+use std::process;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -9,12 +11,26 @@ struct Cli {
     root: bool,
 }
 
+enum Mode {
+    Node,
+    Root,
+}
+
 fn main() {
     let cli = Cli::parse();
+    let mode = if cli.root { Mode::Root } else { Mode::Node };
 
-    if cli.root {
-        println!("Running in root mode");
-    } else {
-        println!("Running in node mode");
+    let _token = require_token();
+
+    match mode {
+        Mode::Root => println!("Running in root mode"),
+        Mode::Node => println!("Running in node mode"),
     }
+}
+
+fn require_token() -> String {
+    env::var("HT_TOKEN").unwrap_or_else(|_| {
+        eprintln!("HT_TOKEN environment variable must be set when running in root or node mode.");
+        process::exit(1);
+    })
 }
