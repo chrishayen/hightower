ce294cf2e76c8200c830f2de13f4c5a005830796
Chris Hayen <chris@shotgun.dev>
2025-10-03 11:51:23 -0400

    --root and --node

--- src/bin/secondary.rs
+++
@@ -1,3 +0,0 @@
-fn main() {
-    println!("Hello from the secondary binary!");
-}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[arg(long, conflicts_with = "root")]
+    node: bool,
+    #[arg(long, conflicts_with = "node")]
+    root: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    if cli.root {
+        println!("Running in root mode");
+    } else {
+        println!("Running in node mode");
+    }
 }
