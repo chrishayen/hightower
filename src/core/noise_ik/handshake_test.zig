69dca6c96be37f0ce2f617c36ce8b92c297289ab
Chris Hayen <chris@shotgun.dev>
2025-10-15 10:32:38 -0400

    encryption / decryption test

--- src/core/noise_ik/handshake_test.zig
+++ src/core/noise_ik/handshake_test.zig
@@ -11,6 +11,8 @@ const initHandshakeInitiator = state_ops.initHandshakeInitiator;
 const initHandshakeResponder = state_ops.initHandshakeResponder;
 const splitCiphers = state_ops.splitCiphers;
 const generateKeyPair = crypto_ops.generateKeyPair;
+const encryptWithAd = crypto_ops.encryptWithAd;
+const decryptWithAd = crypto_ops.decryptWithAd;
 
 test "full noise IK handshake succeeds" {
     const initiator_static = try generateKeyPair();
@@ -182,4 +184,53 @@ test "handshake with large payloads succeeds" {
     const payload_b_len = try readMessageB(&initiator, msg_b[0..msg_b_len], &received_payload_b);
 
     try std.testing.expectEqualSlices(u8, responder_payload, received_payload_b[0..payload_b_len]);
+}
+
+test "post-handshake transport encryption succeeds" {
+    const initiator_static = try generateKeyPair();
+    const responder_static = try generateKeyPair();
+
+    var initiator = initHandshakeInitiator(initiator_static, responder_static.public);
+    var responder = initHandshakeResponder(responder_static);
+
+    const handshake_payload_a = "initiator handshake payload";
+    var msg_a: [1000]u8 = undefined;
+    const msg_a_len = try writeMessageA(&initiator, handshake_payload_a, &msg_a);
+
+    var received_payload_a: [1000]u8 = undefined;
+    const payload_a_len = try readMessageA(&responder, msg_a[0..msg_a_len], &received_payload_a);
+    try std.testing.expectEqualSlices(u8, handshake_payload_a, received_payload_a[0..payload_a_len]);
+
+    const handshake_payload_b = "responder handshake payload";
+    var msg_b: [1000]u8 = undefined;
+    const msg_b_len = try writeMessageB(&responder, handshake_payload_b, &msg_b);
+
+    var received_payload_b: [1000]u8 = undefined;
+    const payload_b_len = try readMessageB(&initiator, msg_b[0..msg_b_len], &received_payload_b);
+    try std.testing.expectEqualSlices(u8, handshake_payload_b, received_payload_b[0..payload_b_len]);
+
+    var initiator_ciphers = splitCiphers(&initiator);
+    var responder_ciphers = splitCiphers(&responder);
+
+    const initiator_message = "message from initiator";
+    var encrypted_from_initiator: [1000]u8 = undefined;
+    const encrypted_len = try encryptWithAd(&initiator_ciphers[0], "", initiator_message, &encrypted_from_initiator);
+    initiator_ciphers[0].nonce += 1;
+
+    var decrypted_at_responder: [1000]u8 = undefined;
+    const decrypted_len = try decryptWithAd(&responder_ciphers[0], "", encrypted_from_initiator[0..encrypted_len], &decrypted_at_responder);
+    responder_ciphers[0].nonce += 1;
+
+    try std.testing.expectEqualSlices(u8, initiator_message, decrypted_at_responder[0..decrypted_len]);
+
+    const responder_message = "message from responder";
+    var encrypted_from_responder: [1000]u8 = undefined;
+    const encrypted_len_resp = try encryptWithAd(&responder_ciphers[1], "", responder_message, &encrypted_from_responder);
+    responder_ciphers[1].nonce += 1;
+
+    var decrypted_at_initiator: [1000]u8 = undefined;
+    const decrypted_len_init = try decryptWithAd(&initiator_ciphers[1], "", encrypted_from_responder[0..encrypted_len_resp], &decrypted_at_initiator);
+    initiator_ciphers[1].nonce += 1;
+
+    try std.testing.expectEqualSlices(u8, responder_message, decrypted_at_initiator[0..decrypted_len_init]);
 }
